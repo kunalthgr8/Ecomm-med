791e9f62a3d4baa654dc8df49239d0c2498da887
kunalthgr8 <kunalsingla@iitbhilai.ac.in>
2024-06-08 11:21:55 +0530

    Real backend Intetgartion started

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,6 +1,6 @@
 import conf from "../conf/conf.js";
 import { Client, Account, ID } from "appwrite";
-
+import axios from "axios";
 export class AuthService {
   client = new Client();
   account;
@@ -14,19 +14,43 @@ export class AuthService {
 
   async createAccount({ username, email, password }) {
     try {
-      console.log("AuthService :: createAccount 111111111 :: data", { username, email, password })
+      console.log("AuthService :: createAccount 111111111 :: data", {
+        username,
+        email,
+        password,
+      });
       const userAccount = await this.account.create(
         ID.unique(),
         email,
         password,
-        username,
+        username
       );
-      console.log("AuthService :: createAccount  222222222222:: data", { username, email, password })
+      console.log("AuthService :: createAccount  222222222222:: data", {
+        username,
+        email,
+        password,
+      });
       if (userAccount) {
         return this.login({ email, password });
       } else {
         return userAccount;
       }
+      // const response = await axios.post(
+      //   "http://localhost:8000/api/v1/users/register",
+      //   {
+      //     fullname: "Kunal Sing",
+      //     username,
+      //     email,
+      //     password,
+      //   }
+      // );
+
+      // if (response.status === 201) {
+      //   // If registration is successful, log in the user
+      //   return this.login({ email, password });
+      // } else {
+      //   throw new Error("User registration failed");
+      // }
     } catch (error) {
       throw error;
     }
@@ -35,6 +59,14 @@ export class AuthService {
   async login({ email, password }) {
     try {
       return await this.account.createEmailPasswordSession(email, password);
+      // const response = await axios.post(
+      //   "http://localhost:8000/api/v1/users/login",
+      //   {
+      //     email,
+      //     password,
+      //   }
+      // );
+      // return response;
     } catch (error) {
       throw error;
     }
@@ -53,6 +85,10 @@ export class AuthService {
   async logout() {
     try {
       await this.account.deleteSessions();
+      // const response = await axios.post(
+      //   "http://localhost:8000/api/v1/users/logout"
+      // );
+      // return response;
     } catch (error) {
       console.log("Appwrite serive :: logout :: error", error);
     }
